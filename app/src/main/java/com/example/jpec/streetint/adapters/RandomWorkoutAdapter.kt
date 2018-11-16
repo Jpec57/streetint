91da3be22cd0df5e95ff26c83d465f0847ccb5d7
Jean-Paul BELLA <jean.paul.bella@hotmail.fr>
2018-11-16 18:34:33 +0100

    Really cool adapter 8)

--- app/src/main/java/com/example/jpec/streetint/adapters/RandomWorkoutAdapter.kt
+++ app/src/main/java/com/example/jpec/streetint/adapters/RandomWorkoutAdapter.kt
@@ -1,18 +1,25 @@
 package com.example.jpec.streetint.adapters
 
 import android.content.Context
+import android.graphics.Color
 import android.support.constraint.ConstraintLayout
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.jpec.streetint.R
 import kotlinx.android.synthetic.main.expandable_parent_random_workout.view.*
+import java.lang.Exception
 
 class RandomWorkoutAdapter(val context: Context, private val choices: MutableMap<String, ArrayList<String>>?) : RecyclerView.Adapter<RandomWorkoutAdapter.MyViewHolder>()
 {
+    private val selectedMap = mutableMapOf<String, Boolean>()
+    private lateinit var parentView: ViewGroup
+
 
     class MyViewHolder(val baseLayout: ConstraintLayout) : RecyclerView.ViewHolder(baseLayout)
 
@@ -20,6 +27,14 @@ class RandomWorkoutAdapter(val context: Context, private val choices: MutableMap
                                     viewType: Int): RandomWorkoutAdapter.MyViewHolder {
         val baseLayout = LayoutInflater.from(parent.context)
             .inflate(R.layout.expandable_parent_random_workout, parent, false) as ConstraintLayout
+        for (key in choices!!.keys)
+        {
+            for (item in choices[key]!!)
+            {
+                selectedMap[item] = false
+            }
+        }
+        parentView = parent
         return MyViewHolder(baseLayout)
     }
     override fun getItemCount() = choices?.size ?: 0
@@ -28,9 +43,11 @@ class RandomWorkoutAdapter(val context: Context, private val choices: MutableMap
         holder.baseLayout.option_name.text = choices!!.keys.elementAt(position)
 
         val viewManager = LinearLayoutManager(context)
-        val viewAdapter = RandomWorkoutInsideAdapter(choices[choices.keys.elementAt(position)])
+        val viewAdapter = RandomWorkoutInsideAdapter(holder.baseLayout.selected, choices[choices.keys.elementAt(position)],
+            choices!!.keys.elementAt(position), this, position > 1)
 
-        holder.baseLayout.recyclerInside.apply {
+        val recycler = holder.baseLayout.recyclerInside
+        recycler.apply {
             // use this setting to improve performance if you know that changes
             // in content do not change the layout size of the RecyclerView
             setHasFixedSize(true)
@@ -44,8 +61,24 @@ class RandomWorkoutAdapter(val context: Context, private val choices: MutableMap
         }
     }
 
-    private fun setAdapter(view: View)
+    fun setPreviewTextAtPos(textView: TextView, position: String)
     {
-
+        var string = ""
+        for (item in choices!![position]!!)
+        {
+            if (selectedMap[item]!!)
+            {
+                string += " $item,"
+            }
+        }
+        string = if (string.length > 1) string.substring(0, string.length - 1) else string
+        textView.text = string
     }
+
+    fun setSelectedAtPos(item: String)
+    {
+        val previousVal = selectedMap[item] as Boolean
+        selectedMap[item] = !previousVal
+    }
+
 }
--- app/src/main/java/com/example/jpec/streetint/adapters/RandomWorkoutInsideAdapter.kt
+++ app/src/main/java/com/example/jpec/streetint/adapters/RandomWorkoutInsideAdapter.kt
@@ -5,11 +5,16 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.jpec.streetint.R
 import kotlinx.android.synthetic.main.expandable_child_random_workout.view.*
 
-class RandomWorkoutInsideAdapter(private val choices: List<String>?) : RecyclerView.Adapter<RandomWorkoutInsideAdapter.MyViewHolder>()
+class RandomWorkoutInsideAdapter(val textView: TextView, private val choices: List<String>?,
+                                 private val optionsName: String, private val randomWorkoutAdapter: RandomWorkoutAdapter,
+                                 private val multiple: Boolean) : RecyclerView.Adapter<RandomWorkoutInsideAdapter.MyViewHolder>()
 {
+    private var oldSelected : Int = -1
+    private var oldSelectedView: View? = null
     class MyViewHolder(val baseLayout: ConstraintLayout) : RecyclerView.ViewHolder(baseLayout)
 
     override fun onCreateViewHolder(parent: ViewGroup,
@@ -23,14 +28,26 @@ class RandomWorkoutInsideAdapter(private val choices: List<String>?) : RecyclerV
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.baseLayout.options.text = choices!![position]
         holder.baseLayout.option_layout.setOnClickListener {
+            if (!multiple)
+            {
+                if (oldSelected != -1)
+                {
+                    randomWorkoutAdapter.setSelectedAtPos(choices[oldSelected])
+                    oldSelectedView!!.visibility = View.INVISIBLE
+
+                }
+            }
             if (holder.baseLayout.chosen.visibility == View.VISIBLE)
-            {
                 holder.baseLayout.chosen.visibility = View.INVISIBLE
-            }
             else
-            {
                 holder.baseLayout.chosen.visibility = View.VISIBLE
+            if (!multiple)
+            {
+                oldSelected = position
+                oldSelectedView = holder.baseLayout.chosen
             }
+            randomWorkoutAdapter.setSelectedAtPos(choices[position])
+            randomWorkoutAdapter.setPreviewTextAtPos(textView, optionsName)
         }
     }
 }
